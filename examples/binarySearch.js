b6546b4d4417a7d91baa4fb23f7936fa19a53e07
ValeryDerbeko <valeryderbeko@gmail.com>
2025-05-13 23:30:11 +0700

    Добавлен алгоритм бинарного поиска

--- examples/binarySearch.js
+++ examples/binarySearch.js
@@ -1,7 +1,34 @@
 import { NAMES } from "../constants/index.js";
 
-function binarySearch() {
-  
+/**
+ * Алгоритм бинарного поиска элемента в массиве
+ * @param {Array} array - массив любых значений 
+ * @param {any} value - элемент массива, который необходимо найти
+ * @returns идекс искомого элемента в массиве
+ */
+function binarySearch(array, value) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((right + left) / 2);
+
+    if (value === array[middle]) return middle;
+
+    if (value > array[middle]) {
+      left = middle + 1;
+    }
+
+    if (value < array[middle]) {
+      right = middle - 1;
+    }
+  }
+
+  return -1;
 }
 
-binarySearch();
+const indexOfCustomFunc = binarySearch(NAMES, "Оксана");
+const index = NAMES.findIndex((x) => x === "Оксана");
+
+console.log("Результат выполнения binarySearch: ", indexOfCustomFunc);
+console.log("Результат выполнения findIndex: ", index);
